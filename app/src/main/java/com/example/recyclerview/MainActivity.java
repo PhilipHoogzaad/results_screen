7f5ae18000eca2a9804ceaf290503be854aecb1b
PhilipHoogzaad <philip98188@hotmail.com>
2020-12-13 17:15:40 +0100

    initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1, s2;
+    int[] images = {R.drawable.kapper1, R.drawable.kapper2, R.drawable.kapper3, R.drawable.kapper4, R.drawable.kapper5, R.drawable.kapper6};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.kapperNaam);
+        s2 = getResources().getStringArray(R.array.slogan);
+
+        MyAdapter myAdapter = new MyAdapter (this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -0,0 +1,75 @@
+package com.example.recyclerview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+    public MyAdapter (Context ct, String s1[],String s2[], int img[]) {
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.kapperNaam.setText(data1[position]);
+        holder.slogan.setText(data2[position]);
+        holder.kapperImg.setImageResource(images[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (context, KapperInfo.class);
+                intent.putExtra("data1", data1[position]);
+                intent.putExtra("data2", data2[position]);
+                intent.putExtra("myImage", images[position]);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView kapperNaam, slogan;
+        ImageView kapperImg;
+
+        ConstraintLayout mainLayout;
+
+        public MyViewHolder (@NonNull View itemView) {
+            super(itemView);
+
+            kapperNaam = itemView.findViewById(R.id.kapperNaam);
+            slogan = itemView.findViewById(R.id.slogan);
+            kapperImg = itemView.findViewById(R.id.kapperImg);
+
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
