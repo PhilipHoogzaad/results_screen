d419bbc5294d0313f4f2dbe332b412f9b0d5b012
PhilipHoogzaad <philip98188@hotmail.com>
2020-12-15 16:49:38 +0100

    Kapper Beschrijving Edit
    App werkt maar laad de string-array niet in textview

--- app/src/main/java/com/example/recyclerview/KapperInfo.java
+++ app/src/main/java/com/example/recyclerview/KapperInfo.java
@@ -5,14 +5,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.w3c.dom.Text;
 
 public class KapperInfo extends AppCompatActivity {
 
-    ImageView mainImageView;
-    TextView title;
-    TextView description;
+    ImageView kapperImg;
+    TextView kapperNaam;
+    TextView slogan;
+    TextView txt1;
 
+    // NOTE: kapper beschrijving heeft String-array: "kapperBeschrijving"
+    // NOTE: beschrijving == s3
+    // NOTE: TextView "txt1" = positie weergeving "kapperBeschrijving" zie "KapperInfo.java"
+
+
+    //String data1, data2, data3;
     String data1, data2;
+    String[] s3;
     int myImage;
 
     @Override
@@ -20,20 +29,25 @@ public class KapperInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kapper_info);
 
-        mainImageView = findViewById(R.id.mainImageView);
-        title = findViewById(R.id.title);
-        description = findViewById(R.id.description);
+
+        kapperImg = findViewById(R.id.kapperImg);
+        kapperNaam = findViewById(R.id.kapperNaam);
+        slogan = findViewById(R.id.slogan);
+        txt1 = findViewById(R.id.txt1);
 
         getData();
         setData();
     }
 
     private void getData() {
+        //if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2") && getIntent().hasExtra("data3")) {
         if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")) {
-
             data1 = getIntent().getStringExtra("data1");
             data2 = getIntent().getStringExtra("data2");
+            //data3 = getIntent().getStringExtra("data3");
             myImage = getIntent().getIntExtra("myImage", 1);
+
+            s3 = getResources().getStringArray(R.array.kapperBeschrijving);
 
         } else {
 
@@ -44,9 +58,11 @@ public class KapperInfo extends AppCompatActivity {
 
     private void setData() {
 
-        title.setText(data1);
-        description.setText(data2);
-        mainImageView.setImageResource(myImage);
+        kapperNaam.setText(data1);
+        slogan.setText(data2);
+        //txt1.setText(data3);
+        txt1.setText(s3);
+        kapperImg.setImageResource(myImage);
 
     }
 }
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.recyclerview;
 
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
+    TextView txt1;
+
+    //String[] s1, s2, s3;
     String[] s1, s2;
     int[] images = {R.drawable.kapper1, R.drawable.kapper2, R.drawable.kapper3, R.drawable.kapper4, R.drawable.kapper5, R.drawable.kapper6};
 
@@ -21,9 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         s1 = getResources().getStringArray(R.array.kapperNaam);
         s2 = getResources().getStringArray(R.array.slogan);
+        //s3 = getResources().getStringArray(R.array.kapperBeschrijving);
 
-        MyAdapter myAdapter = new MyAdapter (this, s1, s2, images);
+        //MyAdapter myAdapter = new MyAdapter (this, s1, s2, s3, images);
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Werkt niet!
+        //txt1.setVisibility(View.GONE);
     }
 }
